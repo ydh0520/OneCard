e51cc37a97a1f1d4f7d8680e9f8d76ea597316a9
YunDongHyeon <ydh2338@naver.com>
2019-04-17 20:21:58 +0900

    basic room function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
    <div>
-     <button @click="make()">wefwef</button>
+     <input type="text" :v-bind="inputtitle">
+     <button @click="makeroom()">방만들기</button>
+     <button @click="removeroom()">방삭제</button>
    </div>
   </div>
 </template>
@@ -14,11 +16,26 @@ export default {
   name: 'home',
   components: {
   },
+  data(){
+    return{
+      inputtitle:'',
+      currentroomid:'',
+    }
+  },
   methods:{
-    make(){
-      firebase.database().ref('/').set({
-        test:'test4'
+    makeroom(){
+      var room=firebase.database().ref('/room').push({
+        title : '',
+        player:['me'],
+        option:{
+          playerlimit:6,
+          rule:'basic'
+        }
       })
+      this.currentroomid=room.key; 
+    },
+    removeroom(){
+      firebase.database().ref('/room/'+this.currentroomid).remove();
     }
   }
 }
