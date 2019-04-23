6bea3066c80ab9c86c697f3fe080075ccbe60cdf
YunDongHyeon <ydh2338@naver.com>
2019-04-23 17:10:43 +0900

    call double

--- src/views/test.vue
+++ src/views/test.vue
@@ -2,32 +2,49 @@
 <div>
     <div>
         <button @click="init()">초기화</button>
-        <button v-if="info.ing==0&&playernum==0" @click="start()">시작</button>
         <button v-if="info==''" @click="enter()">참가</button>
         <div></div>
     </div>
     <div v-if="info.ing==0">
+        <h3>참가자</h3>
+        
         <li v-for="(item,index) in player" :key="index">
             player {{index}}
         </li>
+
+        <button v-if="playernum==0" @click="start()">시작</button>
     </div>
+
     <div v-if="info.ing==1">
-        <li v-for="(item,index) in player" :key="index" >
-            <p>player {{index}}: {{item}}</p>
-        </li>
+        <h1>Player : {{playernum}}</h1>
+        <div>
+            <li v-for="(item,index) in player" :key="index" >
+                <p>player {{index}}: {{item.value}}</p>
+            </li>
+        </div>
+        <div>
+            {{game.totalprice}}
+        </div>
+
+        <div v-if="game.turn==playernum">
+            <button @click="call()">
+                call
+            </button>
+            <button @click="double()">
+                double
+            </button>
+            <button @click="die()">
+                die
+            </button>
+        </div>
+
+        <div v-if="game.turn==game.rise">
+            <button @click="accept()">내가이김</button>
+        </div>
     </div>
-    <div v-if="info.turn==playernum">
-        <button @click="call()">
-            call
-        </button>
-        <button @click="double()">
-            double
-        </button>
-        <button @click="die()">
-            die
-        </button>
-        <button v-if="info.rise==playernum" @click="accept()">정산하기</button>
-    </div>
+
+
+   
 </div>
 </template>
 
@@ -37,11 +54,10 @@ import 'firebase/database'
 export default {
     data(){
         return {
-            info:'',
-            price:'',
             playernum:'',
-            player:'',
-            myValue:200
+            info:'',
+            game:'',
+            player:''
         }
     },
     methods:{
@@ -51,11 +67,14 @@ export default {
                 {
                     'info':{
                         ing:0,
+                        playercounter:1,
+                        gamecounter:0
+                    },
+                    'game':{
                         turn:0,
                         rise:0,
                         totalprice:0,
                         betting:1,
-                        playercounter:1
                     },
                     'player':{
                         0:{
@@ -65,11 +84,14 @@ export default {
                     }
                 }
             );
+
             var $vm=this;
             this.playernum=0;
+
             foker.on('value',function(snapshot){
                 var snapshotData=snapshot.val();
                 $vm.info=snapshotData.info;
+                $vm.game=snapshotData.game;
                 $vm.player=snapshotData.player;
             });
         },
@@ -84,6 +106,7 @@ export default {
             foker.on('value',function(snapshot){
                 var snapshotData=snapshot.val();
                 $vm.info=snapshotData.info;
+                $vm.game=snapshotData.game;
                 $vm.player=snapshotData.player;
             })
         },
@@ -98,32 +121,55 @@ export default {
             firebase.database().ref('/foker/info').update({ing:1});
         },
         call(){
-            this.myValue-=this.info.betting;
-            this.totalprice+=this.info.betting;
-            firebase.database().ref('/foker/player/'+this.playernum).update({
-                state:0,
-                value:this.myValue
-            })
-            this.next();
+            this.player[this.playernum].value-=this.game.betting;
+
+            var next=(this.game.turn+1)%this.info.playercounter;
+            var totalprice=this.game.totalprice+this.game.betting;
+            
+            firebase.database().ref('/foker/player/'+this.playernum).update(this.player[this.playernum]);
+            firebase.database().ref('/foker/game').update({
+                turn:next,
+                totalprice:totalprice
+            });
         },
         double(){
-            var nextrise=this.playernum;
-            var nextbetting = this.info.betting*=2;
-
-            firebase.database().ref('/foker/info').update({rise:nextrise});
-            firebase.database().ref('/foker/info').update({betting:nextbetting});
+            this.game.rise=this.playernum;
+            this.game.betting*=2;
             
+            firebase.database().ref('/foker/game').update(this.game)
+
             this.call();
         },
         die(){
+            this.player[this.playernum].state=1;
+            firebase.database().ref('/foker/player/'+this.playernum).update(this.player[this.playernum]);
             
+            var next=(this.game.turn+1)%this.info.playercounter;
+            firebase.database().ref('/foker/game').update({
+                turn:next
+            });
         },
         accept(){
+            let result = this.player[this.playernum].value+=this.game.totalprice;
 
+            firebase.database().ref('/foker/game').update({
+                betting:1,
+                rise:this.playernum,
+                totalprice:0,
+                turn:this.playernum
+            })
+            this.player[this.playernum].value=result;
+            
+            for(let i=0;i<this.info.playercounter;i++){
+                this.player[i].state=0;
+            }
+            
+            firebase.database().ref('/foker/player').update(this.player);
         },
         next(){
-            let next = (this.info.turn+1)%this.info.playercounter;
-            firebase.database().ref('/foker/info').update({turn:next});
+
+            
+            return 
         }
     }
 }
